08a2d8cb1b70c1f98afe562fc9f07d26ab283f78
rohit-2604 <rohitneogi26042002@gmail.com>
2026-01-18 20:19:23 +0530

    feat: update email address in Contact component

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -112,7 +112,7 @@ export default function Contact() {
                   </div>
                   <div>
                     <h3 className="font-semibold">Email</h3>
-                    <p className="text-gray-400">olovajs@gmail.com</p>
+                    <p className="text-gray-400">test@gmail.com</p>
                   </div>
                 </div>
 
